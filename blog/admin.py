9876d5d16d6802098febe7c8dfceba4dbb2b3876
AnjalBam <anjalbam81@gmail.com>
2020-07-15 03:15:38 +0545

    added models to admin and also modified the admin view

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, BlogPost
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+    search_fields = ('name',)
+
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'published_at', 'last_modified', 'author')
+    search_fields = ('title', 'slug', 'author')
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(BlogPost, BlogPostAdmin)
+
