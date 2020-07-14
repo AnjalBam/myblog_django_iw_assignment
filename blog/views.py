88360aba30e6b50271d9b0bdb52e4f37fcc9eb61
AnjalBam <anjalbam81@gmail.com>
2020-07-15 03:16:22 +0545

    made simple homepage and bootstrapped with bootstrap4

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from django.http import HttpResponse
+from .models import Author, BlogPost
 
 # Create your views here.
+
+
+def home_view(request):
+    posts = get_list_or_404(BlogPost)
+    print(posts)
+    context = {}
+    return render(request, 'blog/index.html', context=context)
