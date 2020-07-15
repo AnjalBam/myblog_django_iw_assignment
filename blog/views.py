570e7fd5003b0d22dc4c1ce548d615cc7e929608
AnjalBam <anjalbam81@gmail.com>
2020-07-15 13:03:56 +0545

    authors are now dynamically rendered from the database

--- blog/views.py
+++ blog/views.py
@@ -7,11 +7,13 @@ from .models import Author, BlogPost
 
 def home_view(request):
     posts = get_list_or_404(BlogPost)
-    print(posts)
+    # print(posts)
     context = {'posts': posts}
     return render(request, 'blog/index.html', context=context)
 
 
 def author_list_view(request):
-    context = {}
+    authors = get_list_or_404(Author)
+    # print(authors)
+    context = { 'authors': authors }
     return render(request, 'blog/author-list.html', context=context)
