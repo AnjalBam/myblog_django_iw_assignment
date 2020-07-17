c0a9f9647da311e53e755aee7a65522255615f35
AnjalBam <anjalbam81@gmail.com>
2020-07-17 21:29:22 +0545

    settings file configured into a package.

--- myBlog_iw/settings/base.py
+++ myBlog_iw/settings/base.py
@@ -19,13 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e*$*#!lkbiv9b-a%#s$+64e41dcs@rii8+=-7dx@uzk$07!-gs'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -117,13 +111,3 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
-
-STATIC_ROOT = '/assets/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
